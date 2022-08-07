d513890df5901a204ec21218cdbbf30d88e69baf
Markus Hinz <info@markushinz.de>
2022-08-07 10:26:38 +0200

    test version

--- version/version.sh
+++ version/version.sh
@@ -16,5 +16,20 @@ if [[ ${latest_tag:1} =~ ([0-9]+).([0-9]+).([0-9]+) ]]; then
   patch="${BASH_REMATCH[3]}"
 fi
 
-version=$(echo -e "${major}.${minor}.$((patch + 1))\n${minimum_version}" | sort -V | tail -1)
+GITHUB_EVENT_PATH=${GITHUB_EVENT_PATH:-"$(dirname "${0}")/event.json"}
+if jq -e '[.pull_request.labels[].name] | index("major")' "${GITHUB_EVENT_PATH}" &> /dev/null; then
+  major="$((major + 1))"
+  minor="0"
+  patch="0"
+elif jq -e '[.pull_request.labels[].name] | index("minor")' "${GITHUB_EVENT_PATH}" &> /dev/null; then
+  minor="$((minor + 1))"
+  patch="0"
+elif jq -e '[.pull_request.labels[].name] | index("chore")' "${GITHUB_EVENT_PATH}" &> /dev/null; then
+  echo "null"
+  exit 0
+else
+  patch="$((patch + 1))"
+fi
+
+version=$(echo -e "${major}.${minor}.${patch}\n${minimum_version}" | sort -V | tail -1)
 echo "${version}"
