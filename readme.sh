fef632568c6569f3ee4eb0f325282fbd570cc361
Markus Hinz <info@markushinz.de>
2022-09-09 22:13:52 +0200

    readme

--- readme.sh
+++ readme.sh
@@ -2,12 +2,10 @@
 
 set -eou pipefail
 
-url="$(gh repo view --json url --jq .url)"
-
 cat << EOF > README.md
 # actions
 
-[![CI](${url}/actions/workflows/ci.yaml/badge.svg)](${url}/actions/workflows/ci.yaml)
+[![CI](https://github.com/markushinz/actions/actions/workflows/ci.yaml/badge.svg)](https://github.com/markushinz/actions/actions/workflows/ci.yaml)
 
 A collection of reusable GitHub Actions.
 
